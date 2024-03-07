71abe0ee97b7dc4d93fddce7c14624a25a498c88
Shon Kaganovich <kaga1@umbc.edu>
2024-03-07 15:50:41 -0500

    connected server with db and front end

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, jsonify
+import sqlite3
+from db.db import init_db
+
+# create flask app 
+app = Flask(__name__)
+
+# start the db 
+init_db()
+
+def get_tasks():
+    conn = sqlite3.connect('db/database.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM tasks')
+    tasks = cur.fetchall()
+    cur.close()
+    conn.close()
+    return tasks
+
+# Route to display tasks
+@app.route('/')
+def show_tasks():
+    tasks = get_tasks()
+    return render_template('index.html', tasks=tasks)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
